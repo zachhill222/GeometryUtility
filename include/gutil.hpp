d80d053687903e57c3e427069421bcf861e3a725
zachhill222 <zach.hill222@gmail.com>
2026-07-23 16:53:46 -0400

    updated headers

--- include/gutil.hpp
+++ include/gutil.hpp
@@ -3,21 +3,11 @@
 #include "math/math.hpp"
 #include "utility/utility.hpp"
 #include "geometry/geometry.hpp"
-
-// #include "octree/node.hpp"
-// #include "octree/octree_base.hpp"
-// #include "octree/point_octree.hpp"
+#include "shapes/shapes.hpp"
 
 #include "octree/point_octree.hpp"
+#include "octree/volume_octree.hpp"
 
-#include "memory/slab_allocator.hpp"
-#include "memory/hetero_slab_allocator.hpp"
 #include "memory/thread_pool.hpp"
 
 #include "algorithms/convex_collision.hpp"
-
-// namespace gutil
-// {
-// 	template<int offset=0>
-// 	using FixedPoint64 = FixedPoint<int64_t,offset>;
-// }
