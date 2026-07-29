07ce70300d6ef87a88c9867af50792e4836254e2
zachhill222 <zach.hill222@gmail.com>
2026-07-29 15:14:46 -0400

    added stack trace to GUTIL_ASSERT

--- include/utility/assert.hpp
+++ include/utility/assert.hpp
@@ -6,6 +6,12 @@
 #include <string_view>
 #include <source_location>
 
+#ifdef __cpp_lib_stacktrace
+	#include <stacktrace>
+	#define GUTIL_HAS_STACKTRACE(...) __VA_ARGS__
+#else
+	#define GUTIL_HAS_STACKTRACE(...)
+#endif
 
 #ifndef NDEBUG
 	#define GUTIL_ASSERT(cond) gutil::gutil_assert(cond, #cond);
@@ -17,20 +23,24 @@
 
 namespace gutil {
 	inline void gutil_assert(bool cond, std::string_view condition_str,
-			std::source_location loc = std::source_location::current() ) noexcept {
+			std::source_location loc = std::source_location::current()
+			GUTIL_HAS_STACKTRACE(,std::stacktrace trace = std::stacktrace::current()) ) noexcept {
 		if (cond) { return; }
 
 		Logger::error("GUTIL_ASSERT : ",condition_str,"\n",
-						 "\tat ",loc.file_name()," : ",loc.line()," : ",loc.column(),"\n",
-						 "\tin ",loc.function_name());
+						 "\tat ",loc.file_name()," line: ",loc.line()," col: ",loc.column(),"\n",
+						 "\tin ",loc.function_name()
+						 GUTIL_HAS_STACKTRACE(,"\tstacktrace:", trace));
 		std::abort();
 	}
 
 	inline void gutil_abort(std::string_view condition_str,
-			std::source_location loc = std::source_location::current() ) noexcept {
+			std::source_location loc = std::source_location::current()
+			GUTIL_HAS_STACKTRACE(,std::stacktrace trace = std::stacktrace::current()) ) noexcept {
 		Logger::error("GUTIL_ABORT : ",condition_str,"\n",
 						 "\tat ",loc.file_name()," : ",loc.line()," : ",loc.column(),"\n",
-						 "\tin ",loc.function_name());
+						 "\tin ",loc.function_name()
+						 GUTIL_HAS_STACKTRACE(,"\tstacktrace:", trace));
 		std::abort();
 	}
 }
